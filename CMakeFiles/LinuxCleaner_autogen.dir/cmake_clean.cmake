37f0b6505b8734c7224fd8a5214108a457185059
stellar-store <gus.bonachea@outlook.com>
2026-03-18 23:59:29 -0300

    Add files via upload

---
+++ CMakeFiles/LinuxCleaner_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinuxCleaner_autogen"
+  "LinuxCleaner_autogen/mocs_compilation.cpp"
+  "LinuxCleaner_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/LinuxCleaner_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
